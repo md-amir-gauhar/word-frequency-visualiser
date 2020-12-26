80e7ffe4cf6d20bb8bfeab09287ce45537fb465a
md-amir-gauhar <gauharamir15@gmail.com>
2020-12-26 20:12:28 +0530

    final commit

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ btnCalc.addEventListener('click', () => {
   // console.log(inputTxt.value);
   let text = inputTxt.value;
   let words = getWords(text);
-  console.log(words);
+  let wc = getWordCounts(words);
+  let wcArr = sortWordCounts(wc);
+  printWordCountTable(wcArr);
+  generateChart(wcArr);
 });
 
 const getWords = (inputText) => {
@@ -41,5 +44,83 @@ const getWords = (inputText) => {
   });
   let newText = newChars.join('');
   let words = newText.split(' ');
-  return newText;
+  return words;
+};
+
+const getWordCounts = (words) => {
+  let wordCounts = {};
+  words.forEach((word) => {
+    if (wordCounts[word]) {
+      wordCounts[word]++;
+    } else {
+      wordCounts[word] = 1;
+    }
+  });
+
+  return wordCounts;
+};
+
+const sortWordCounts = (wordCounts) => {
+  let wcArr = [];
+  Object.keys(wordCounts).forEach((wc) => {
+    if (wc == '') return;
+    wcArr.push({
+      word: wc,
+      count: wordCounts[wc],
+    });
+  });
+
+  return wcArr.sort((a, b) => b.count - a.count).slice(0, 50);
+};
+
+const printWordCountTable = (wordCountArray) => {
+  document.getElementById('tableWordCount').innerHTML = `
+    <tr>
+      <th>Word</th>
+      <th>Count</th>
+    </tr>
+  `;
+  wordCountArray.forEach((wc) => {
+    let table = `
+      
+      <tr>
+        <td>${wc.word}</td>
+        <td>${wc.count}</td>
+      </tr>
+    `;
+    document.getElementById('tableWordCount').innerHTML += table;
+  });
+};
+
+const generateChart = (wcArr) => {
+  let ctx = document.getElementById('cvsWcChart').getContext('2d');
+  let chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: wcArr.map((wc) => wc.word),
+      datasets: [
+        {
+          label: 'Word Frequency',
+          data: wcArr.map((wc) => wc.count),
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+          ],
+          borderWidth: 1,
+        },
+      ],
+    },
+  });
 };
